ac72ef3fa7bbe02572b0f9e28c21c728a37704df
Michela <micheladebellis@gmail.com>
2024-04-17 10:37:17 +0200

    Delete a class not used

--- excelCreation/classes.py
+++ excelCreation/classes.py
@@ -12,12 +12,3 @@ class Tariff:
     id: str
     name : str
     discountPercentage : int
-
-@dataclass
-class ExamTariffesSpreadsheet:
-    id_exam : str
-    name_exam : str
-    id_tariff : str
-    name_tariff : str
-    price : float
-
